48fcef9eb681dd7c16d8777b4969b093de84f24b
aalbalushi <46789107+aalbalushi@users.noreply.github.com>
2019-01-17 10:28:59 -0600

    Add files via upload

---
+++ hello (1).c
@@ -0,0 +1,16 @@
+/**
+ * Author: Ahmed Albalushi
+ * Date: 2010/01/17
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Electrical Engineering!\n");
+
+  return 0;
+}
